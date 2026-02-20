051131545666c58fb2d3c5805c60809f908393be
petra <pcvijetinovic@student.tgn.ac.at>
2026-02-20 10:21:09 +0100

    Test class

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Number Stack Test");
+
+            GenericStack<Integer> stack = new GenericStack<>(3);
+
+            stack.push(5);
+            stack.push(10);
+            stack.push(15);
+
+            System.out.println("Stack: " + stack.list());
+
+            System.out.println("Peek: " + stack.peek());
+
+            System.out.println("Pop: " + stack.pop());
+
+            System.out.println("After pop: " + stack.list());
+
+        } catch (StackFullException e) {
+            System.out.println("Stack is full!");
+        } catch (StackEmptyException e) {
+            System.out.println("Stack is empty!");
+        }
+
+        System.out.println(" ");
+
+        try {
+            System.out.println("Text Stack Test");
+
+            GenericStack<String> textStack = new GenericStack<>(2);
+
+            textStack.push("Hello");
+            textStack.push("World");
+
+            System.out.println("Stack: " + textStack.list());
+
+            System.out.println("Pop: " + textStack.pop());
+            System.out.println("Pop: " + textStack.pop());
+
+            // This will cause an exception because the stack is empty
+            textStack.pop();
+
+        } catch (StackFullException e) {
+            System.out.println("Stack is full");
+        } catch (StackEmptyException e) {
+            System.out.println("Stack is empty");
         }
     }
 }
